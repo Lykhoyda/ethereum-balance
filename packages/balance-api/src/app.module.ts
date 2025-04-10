e44f6771bfbce6d05fefe2263ce4fbbe59a16f48
Anton Lykhoyda <lykhoyda@gmail.com>
2025-04-10 14:51:20 +0200

    Add ThrottlerGuard

--- packages/balance-api/src/app.module.ts
+++ packages/balance-api/src/app.module.ts
@@ -2,8 +2,26 @@ import { Module } from '@nestjs/common';
 import { RpcModule } from './rpc/rpc.module';
 import { BalanceModule } from './balance/balance.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [ConfigModule.forRoot(), RpcModule, BalanceModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // time-to-live in milliseconds
+        limit: 50,
+      },
+    ]),
+    RpcModule,
+    BalanceModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
